a7bbf0342fe22e7925d9ab21d870c1eeb0636809
Hannah Eason <silentvalley.hannah@gmail.com>
2022-11-12 15:14:31 -0600

    Added form in login route

--- hackutd/app/routes.py
+++ hackutd/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -11,8 +12,11 @@ def index():
 def explore():
     return render_template("explore.html", title="Explore")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
     return render_template("login.html", title="Login")
 
 @app.route("/logout")
