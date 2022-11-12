4b720c699271254cbc2a9ebc84586fae100f2be0
Ambika E <ambika@rosalogia.me>
2022-11-12 16:56:42 -0600

    Add database models, migations, and file (#1)
    * Fixed post repr in models
    
    * added tag and followers dabatse, as well as repr for all the classes
    
    * Added likes table
    
    * Added database and migration
    
    Co-authored-by: allisonschiang <allisonsc123@gmail.com>

--- hackutd/app/models.py
+++ hackutd/app/models.py
@@ -8,6 +8,8 @@ class User(db.Model):
     bio = Column(String)
     password_hash = Column(String, nullable=False)
     is_artist = Column(Boolean)
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 class Post(db.Model):
     id = Column(Integer, primary_key=True)
@@ -16,7 +18,28 @@ class Post(db.Model):
     artist_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     posted_at = Column(DateTime, nullable=False)
     price = Column(Float)
+    def __repr__(self):
+        return f"<Post {self.id}>"
+
+class Tag(db.Model):
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    tag = Column(String, nullable=False )
+    def __repr__(self):
+        return f"<Tag {self.id}>"
+
+class Followers(db.Model):
+    id = Column(Integer, primary_key=True)
+    follower = Column(Integer, ForeignKey('user.id'), nullable=False)
+    followee = Column(Integer, ForeignKey('user.id'), nullable = False)
+    def __repr__(self):
+        return f"<Followers {self.id}>"
+
+
+class Likes(db.Model):
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
     def __repr__(self):
-        return f"<User {self.username}>"
-
+        return f"<Like {self.id}"
