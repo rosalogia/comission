4a15d8f316873b4a5295d3773c3a81086fa4601b
yams-yams <uma@kothuri.net>
2022-11-12 15:42:26 -0600

    Added registration form

--- hackutd/app/forms.py
+++ hackutd/app/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
    username = StringField('Username', validators=[DataRequired()])
    password = PasswordField('Password', validators=[DataRequired()])
    remember_me = BooleanField('Remember Me')
    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+   username = StringField('Username', validators=[DataRequired()])
+   password = PasswordField('Password', validators=[DataRequired()])
+   password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+   submit = SubmitField('Register')
--- hackutd/app/routes.py
+++ hackutd/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 
 @app.route("/")
 @app.route("/index")
@@ -29,4 +29,7 @@ def profile():
 
 @app.route("/register")
 def register():
-    return render_template("register.html", title="Register")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template("register.html", title="Register", form=form)
